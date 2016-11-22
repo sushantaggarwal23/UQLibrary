09eb0d61ec8be4b402fa24f6aaff26d4e5474775
sushantaggarwal23 <sushant.aggarwal23@gmail.com>
2016-11-22 14:41:06 +1000

    Update getLibraryList.js

--- webapp/services/getLibraryList.js
+++ webapp/services/getLibraryList.js
@@ -2,6 +2,8 @@
  * Created by Sushant Aggarwal on 20/11/2016.
  */
 
+
+//service for getting library list data
 angular.module('libraryApp').service('getLibraryList', function($http) {
     this.getData = function() {
         var url = 'http://localhost:1337/app.library.uq.edu.au/api/v2/library_hours';
